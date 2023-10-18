a98c46d633a3ad474f807e4ad7796e701b3e8156
Oswald-OSEI <oswaldosei9@gmail.com>
2023-10-18 12:32:49 +0000

    allowed hosts

--- blogProject/blogProject/settings.py
+++ blogProject/blogProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zk9s&m)rj%)=73ir1dd$1!cgbdg&c(dbd7a$9wj_4gu=o%zf1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [" django-env.eba-gupq4fwt.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
