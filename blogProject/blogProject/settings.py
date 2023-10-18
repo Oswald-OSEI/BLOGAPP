9cf45145ce4697908f85dd3338e77d1db01d3f8b
Oswald-OSEI <oswaldosei9@gmail.com>
2023-10-18 13:20:53 +0000

    changes made

--- blogProject/blogProject/settings.py
+++ blogProject/blogProject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zk9s&m)rj%)=73ir1dd$1!cgbdg&c(dbd7a$9wj_4gu=o%zf1='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [" django-env.eba-gupq4fwt.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
