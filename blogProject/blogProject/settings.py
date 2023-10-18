603e710335ab6684d0aeef755297990f0bc806aa
Oswald-OSEI <oswaldosei9@gmail.com>
2023-10-18 14:50:04 +0000

    deployed

--- blogProject/blogProject/settings.py
+++ blogProject/blogProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["blogProject-env.eba-en3mvhme.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
